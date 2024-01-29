59017666fe7fff8cff3cde4ac123cec3b04743d4
Shincy <shincyshnz@gmail.com>
2024-01-29 15:19:08 +0400

    implement the update

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2146,7 +2146,6 @@ class View {
         this._parentElement.insertAdjacentHTML("afterbegin", markup);
     }
     update(data) {
-        if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const newMarkup = this._generateMarkup();
         const newDOM = document.createRange().createContextualFragment(newMarkup);
